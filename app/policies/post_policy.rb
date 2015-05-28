78fbcfe04c95a5b75b703651e028bf5e1a0e1cc4
Mike Roberts <mikeroberts801@gmail.com>
2015-05-27 19:48:26 -0600

    Assignment 33 completed

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,13 @@ class PostPolicy < ApplicationPolicy
     true
   end
 
+  class Scope < Scope
+    def resolve
+      if !user.blank? &&  (user.admin? || user.moderator?)
+        scope.all?
+      else 
+        scope.where(user: user)
+      end
+    end
+  end
 end
