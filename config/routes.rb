801ea75b3cf6e7badea544b5629e05bc3d3407e4
Mike Roberts <mikeroberts801@gmail.com>
2015-05-30 14:08:08 -0600

    Almost working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'summaries/new'
+
+  get 'summaries/show'
+
   devise_for :users
 
   resources :topics do
-     resources :posts, except: [:index]
+     resources :posts, except: [:index] do
+      resources :summaries, only: [:new, :create, :show]
    end
+ end
 
   get 'about' => 'welcome#about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530181152) do
+ActiveRecord::Schema.define(version: 20150530190220) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 20150530181152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "summaries", force: :cascade do |t|
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "summaries", ["post_id"], name: "index_summaries_on_post_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
