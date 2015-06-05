95fe15db6db908de7313ee90baae538b93038e96
Mike Roberts <mikeroberts801@gmail.com>
2015-06-04 21:15:41 -0600

    Voting assignment complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -32,4 +32,11 @@ def update_rank
   validates :body, length: { minimum: 20 }, presence: true
   #validates :topic, presence: true
   #validates :user, presence: true
+  after_create :create_vote
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+
 end
