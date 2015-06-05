2e134d1551d5344e2ab2a18dc7878f3d38549a3a
Mike Roberts <mikeroberts801@gmail.com>
2015-06-04 21:05:19 -0600

    Checkpoint 49 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   
   has_many :posts
   has_many :comments
+  has_many :votes, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
 
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603002449) do
+ActiveRecord::Schema.define(version: 20150605025535) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "body"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150603002449) do
     t.integer  "user_id"
     t.integer  "topic_id"
     t.string   "image"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -94,5 +95,27 @@ ActiveRecord::Schema.define(version: 20150603002449) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "values", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "values", ["post_id"], name: "index_values_on_post_id"
+  add_index "values", ["user_id"], name: "index_values_on_user_id"
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
