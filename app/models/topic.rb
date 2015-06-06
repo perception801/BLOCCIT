75d1f3252e095a88a7b26b2527a3cdcfb56ee917
Mike Roberts <mikeroberts801@gmail.com>
2015-06-06 13:48:00 -0600

    Assignment 52 Complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,11 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  
+  scope :publicy_viewable, -> {where( public: true ) }
+  scope :privately_viewable, -> {where( public: false ) } 
+  scope :visible_to, -> (user) { user ? all : publicy_viewable) }
 
+
+  
 end
