8a2cdd4bd5d651591875b362c2951489f57745cb
Mike Roberts <mikeroberts801@gmail.com>
2015-06-06 13:30:47 -0600

    Checkpoint 52 Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @comments = @post.comments
     @comment = Comment.new
+    authorize @topic
   end
 
   def new
